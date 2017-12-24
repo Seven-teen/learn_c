93bc75b86a6fb27091ed3bc7a25ef988caa01d9d
yihua@ubuntu <yihua@ubuntu>
2017-12-24 20:23:10 +0800

    pointer

---
+++ pointer.c
@@ -0,0 +1,15 @@
+/**
+ * c 指针
+ */
+#include <stdio.h>
+
+int main()
+{
+	int var1;
+	int var2[10];
+	
+	printf("var1 变量的地址：%p\n", &var1);
+	printf("var2 变量的地址：%p\n", &var2);
+	
+	return 0;
+}
---
+++ pointer3.c
@@ -0,0 +1,12 @@
+/**
+ * C中的NULL指针
+ */ 
+#include <stdio.h>
+
+int main()
+{
+	int *ptr = NULL;
+	printf("ptr 的值是 %p\n", ptr);
+
+	return 0;
+}
